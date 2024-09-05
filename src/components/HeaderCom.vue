06be0197d6862ac9c6bec913a341ca33ee389a13
amol <amolpalve181191@gmail.com>
2024-09-05 22:54:18 +0530

    add and update file

--- src/components/HeaderCom.vue
+++ src/components/HeaderCom.vue
@@ -1,9 +1,9 @@
 <template>
 
 <div class="nav">
-<a href="#">Home </a>
-<a href="#">Add Restaurant </a>
-<a href="#">Update Restaurant </a>
+<router-link to="/">Home </router-link>
+<router-link to="AddResto">Add Restaurant </router-link>
+<router-link to="UpdateResto">Update Restaurant </router-link>
 <a v-on:click="logout" href="#">Logout </a>
 </div>
 </template>
--- src/components/HomeName.vue
+++ src/components/HomeName.vue
@@ -1,22 +1,41 @@
 <template>
 <HeaderCom />
-<h1> Welcome home page </h1>
+<h1>Hello {{name}}, Welcome home page </h1>
+<table>
+<tr v-for="item in RestoBar" :key="item.id">
+<td>{{item.id}}</td>
+<td>{{item.name}}</td>
+<td>{{item.address}}</td>
+<td>{{item.contact}}</td>
+</tr>
+</table>
 
 </template>
 
 <script>
+import axios from 'axios'
 import HeaderCom from './HeaderCom.vue'
 export default {
     name: 'HomeName',
+    data(){
+        return{
+            name:'',
+            RestoBar:[],
+        }
+    },
     components: {
 HeaderCom
     },
 
- mounted(){
+ async mounted(){
       let user = localStorage.getItem('user.info');
+      this.name= JSON.parse(user).name
       if(!user){
           this.$router.push({name:'SignUp'})
       }
+      let result = await axios.get("http://localhost:3000/RestoBar");
+      console.warn(result);
+      this.RestoBar = result.data
     }
 }
 </script>
